0878d6003fdb559be093497fd60e9ba01a356b34
Pr4mpox <114173536+Brajamustii@users.noreply.github.com>
2024-11-20 22:16:50 +0700

    Update autotxtaikos2_v2.py

--- autotxtaikos2_v2.py
+++ autotxtaikos2_v2.py
@@ -29,6 +29,10 @@ voteaddr = web3.to_checksum_address("0x4D1E2145082d0AB0fDa4a973dC4887C7295e21aB"
 voteabi = json.loads('[{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"vote","outputs":[],"stateMutability":"payable","type":"function"}]')
 vote_contract = web3.eth.contract(address=voteaddr, abi=voteabi)
 
+# Input manual untuk jeda waktu antar transaksi dalam menit
+delay_minutes = int(input("Input jeda waktu transaksi dalam menit: "))
+delay_seconds = delay_minutes * 60
+
 def get_next_reset_time():
     """Get the next scheduled reset time (7:01 AM Jakarta time)."""
     now = datetime.now(LOCAL_TIMEZONE)
@@ -61,28 +65,28 @@ def vote(wallet, key):
         print(f"Maximum daily transaction limit of {MAX_TX_PER_DAY} reached.")
         return
     try:
-        for i in range(0,15):
-            nonce = web3.eth.get_transaction_count(wallet)+i
-            gasAmount = vote_contract.functions.vote().estimate_gas({
-                'chainId': chainId,
-                'from': wallet
-            })
-            gasPrice = 5050/gasAmount
-            votetx = vote_contract.functions.vote().build_transaction({
-                'chainId': chainId,
-                'from': wallet,
-                'gas': gasAmount,
-                'maxFeePerGas': web3.to_wei(gasPrice, 'gwei'),
-                'maxPriorityFeePerGas': web3.to_wei(gasPrice, 'gwei'),
-                'nonce': nonce
-            })
-            #sign & send the transaction
-            print(f'Processing Vote For Wallet Address {wallet} ...')
-            tx_hash = web3.eth.send_raw_transaction(web3.eth.account.sign_transaction(votetx, key).rawTransaction)
-            print(f'Processing Vote Success!')
-            tx_count_today += 1
-            print(f'TX-ID : {str(web3.to_hex(tx_hash))}')
-            print(f'')
+        nonce = web3.eth.get_transaction_count(wallet)
+        gasAmount = vote_contract.functions.vote().estimate_gas({
+            'chainId': chainId,
+            'from': wallet
+        })
+        gasPrice = 5050/gasAmount
+        votetx = vote_contract.functions.vote().build_transaction({
+            'chainId': chainId,
+            'from': wallet,
+            'gas': gasAmount,
+            'maxFeePerGas': web3.to_wei(gasPrice, 'gwei'),
+            'maxPriorityFeePerGas': web3.to_wei(gasPrice, 'gwei'),
+            'nonce': nonce
+        })
+        #sign & send the transaction
+        print(f'Processing Vote For Wallet Address {wallet} ...')
+        tx_hash = web3.eth.send_raw_transaction(web3.eth.account.sign_transaction(votetx, key).rawTransaction)
+        print(f'Processing Vote Success!')
+        tx_count_today += 1
+        print(f'TX-ID : {str(web3.to_hex(tx_hash))}')
+        print(f'')
+        time.sleep(delay_seconds)
     except Exception as e:
         print(f'Error: {e}')
         print(f'Will Try Again...')
